5d0c6f96c1c338237492a1a7511a6dba96b08a0f
Tin Serdar <serdar.tin@gmail.com>
2026-02-24 08:44:57 +0100

    Initial Commit

---
+++ CheatsheetMD/CheatsheetMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheatsheetMDApp.swift
+//  CheatsheetMD
+//
+//  Created by Tin on 24.02.2026..
+//
+
+import SwiftUI
+
+@main
+struct CheatsheetMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
